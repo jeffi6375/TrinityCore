b31ad69450783e5f4ce2135fe63fe0881ac7dda3
Jeffi <1277179+jeffi6375@users.noreply.github.com>
2025-04-18 13:33:37 +1000

    Delete new loot before inserting

--- sql/custom/world/creature_loot_template.sql
+++ sql/custom/world/creature_loot_template.sql
@@ -1,4 +1,4 @@
 -- Remove Plans: Copper Chain Vest from Siltfin Murloc and add reference loot table for plans
-DELETE FROM `creature_loot_template` WHERE `Entry` = 17190 AND `Item` = 3609;
+DELETE FROM `creature_loot_template` WHERE `Entry` = 17190 AND (`Item` = 3609 OR `Item` = 24700);
 INSERT INTO `creature_loot_template` (`Entry`,`Item`,`Reference`,`Chance`,`QuestRequired`,`LootMode`,`GroupId`,`MinCount`,`MaxCount`,`Comment`)
 VALUES (17190,24700,24700,1.0,0,1,1,1,1,NULL);
